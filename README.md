1dde76d4b23fc269f3e711a7e0b8d95903e6b234
gannacon <cwgannaway@gmail.com>
2021-04-09 22:48:05 -0500

    adjust READ ME and heavy code additions with starting quiz

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
 # Homework_04_Quiz
+
+
+# <BTCMP Homework 4>
+
+## Description
+This javascript will randomly generate a password for you... But you must remember that there is no such thing as random for computers.
+## Installation
+You do not need anything special to run this functioning code. 
+Link for deployed repo: https://gannacon.github.io/Homework-03-PasswordGen/
+## Assets
+Below is a screenshot of the home page.
+
+ ![screenshot of my work](./images/screenshot.png)
+
+## Credits
+I give credit to Byron and Chris who helped me understand HTML and CSS. This code was from scratch but they were a valuable resource when completing this assignment.
+
+## My Task
+
+This week’s homework requires you to modify starter code to create an application that enables employees to generate random passwords based on criteria that they’ve selected. This app will run in the browser and will feature dynamically updated HTML and CSS powered by JavaScript code that you write. It will have a clean and polished, responsive user interface that adapts to multiple screen sizes.
+
+The password can include special characters. If you’re unfamiliar with these, see this [list of password special characters](https://www.owasp.org/index.php/Password_special_characters) from the OWASP Foundation.
+
+
+## User Story
+
+```
+AS A coding boot camp student
+I WANT to take a timed quiz on JavaScript fundamentals that stores high scores
+SO THAT I can gauge my progress compared to my peers
+```
+
+## Acceptance Criteria
+
+```
+GIVEN I am taking a code quiz
+WHEN I click the start button
+THEN a timer starts and I am presented with a question
+WHEN I answer a question
+THEN I am presented with another question
+WHEN I answer a question incorrectly
+THEN time is subtracted from the clock
+WHEN all questions are answered or the timer reaches 0
+THEN the game is over
+WHEN the game is over
+THEN I can save my initials and my score
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,205 @@
-var tipEl = document.querySelector("#tip-percentage");
-var totalEl = document.querySelector("#total");
-var submitEl = document.querySelector("#submit");
+//---------------------------------------------------------------------------
+//DECLARING VARIABLES
+var startButton = document.getElementById('start');
+var timerEl = document.getElementById('time');
 
-function calculateTip(total, tipPercentage) {
-  var roundedResult = (total * tipPercentage).toFixed(2);
-  return roundedResult;
+
+//---------------------------------------------------------------------------
+//FUNCTION TO START TIMER
+startButton.addEventListener('click', function(event) {
+  var timeLeft = 10;
+  event.preventDefault;
+  // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
+  var timeInterval = setInterval(function () {
+    // As long as the `timeLeft` is greater than 1
+    if (timeLeft > 1) {
+      // Set the `textContent` of `timerEl` to show the remaining seconds
+      timerEl.textContent = timeLeft + ' seconds remaining';
+      // Decrement `timeLeft` by 1
+      timeLeft--;
+    } else if (timeLeft === 1) {
+      // When `timeLeft` is equal to 1, rename to 'second' instead of 'seconds'
+      timerEl.textContent = timeLeft + ' second remaining';
+      timeLeft--;
+    } else {
+      // Once `timeLeft` gets to 0, set `timerEl` to an empty string
+      timerEl.textContent = '';
+      // Use `clearInterval()` to stop the timer
+      clearInterval(timeInterval);
+    }
+  }, 1000);
+});
+
+//---------------------------------------------------------------------------
+//FUNCTION TO GENERATE QUESTIONS AND ANSWERS
+function buildQuiz(){
+  // create variable to store the output
+  const output = [];
+
+  output.push(
+    `<div class="slide">
+      <div class="question"> ${currentQuestion.question} </div>
+      <div class="answers"> ${answers.join("")} </div>
+    </div>`
+  );
+
+  // for each question
+  myQuestions.forEach(
+    (currentQuestion, questionNumber) => {
+
+      // variable to store the list of possible answers
+      const answers = [];
+
+      // and for each available answer...
+      for(letter in currentQuestion.answers){
+
+        // ...add an HTML radio button
+        answers.push(
+          `<label>
+            <input type="radio" name="question${questionNumber}" value="${letter}">
+            ${letter} :
+            ${currentQuestion.answers[letter]}
+          </label>`
+        );
+      }
+
+      // add this question and its answers to the output
+      output.push(
+        `<div class="question"> ${currentQuestion.question} </div>
+        <div class="answers"> ${answers.join('')} </div>`
+      );
+    }
+  );
+
+  // finally combine our output list into one string of HTML and put it on the page
+  quizContainer.innerHTML = output.join('');
 }
 
-function calculateTotal(total, tipAmount) {
-  return parseFloat(total) + parseFloat(tipAmount);
+
+//---------------------------------------------------------------------------
+//FUNCTION TO SHOW RESULTS PAGE
+function showResults(){
+
+  // gather answer containers from our quiz
+  const answerContainers = quizContainer.querySelectorAll('.answers');
+
+  // keep track of user's answers
+  let numCorrect = 0;
+
+  // for each question...
+  myQuestions.forEach( (currentQuestion, questionNumber) => {
+
+    // find selected answer
+    const answerContainer = answerContainers[questionNumber];
+    const selector = `input[name=question${questionNumber}]:checked`;
+    const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+
+    // if answer is correct
+    if(userAnswer === currentQuestion.correctAnswer){
+      // add to the number of correct answers
+      numCorrect++;
+
+      // color the answers green
+      answerContainers[questionNumber].style.color = 'lightgreen';
+    }
+    // if answer is wrong or blank
+    else{
+      // color the answers red
+      answerContainers[questionNumber].style.color = 'red';
+    }
+  });
+
+  // show number of correct answers out of total
+  resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
 }
 
-function addTip(event) {
-  // Prevent default action
-  event.preventDefault();
-  // Store tip and total calculations in variables
-  var tipPercentage = tipEl.value * .01;
-  var total = totalEl.value;
-  var tipAmount = calculateTip(total, tipPercentage);
-  var newTotal = calculateTotal(tipAmount, total);
-  // Update HTML with tip and total
-  document.querySelector("#tip-amount").textContent = tipAmount;
-  // Use toFixed() to limit to two decimal places
-  document.querySelector("#new-total").textContent = newTotal.toFixed(2);
+//---------------------------------------------------------------------------
+//FUNCTION TO SHOW SLIDES
+function showSlide(n) {
+  slides[currentSlide].classList.remove('active-slide');
+  slides[n].classList.add('active-slide');
+  currentSlide = n;
+  if(currentSlide === 0){
+    previousButton.style.display = 'none';
+  }
+  else{
+    previousButton.style.display = 'inline-block';
+  }
+  if(currentSlide === slides.length-1){
+    nextButton.style.display = 'none';
+    submitButton.style.display = 'inline-block';
+  }
+  else{
+    nextButton.style.display = 'inline-block';
+    submitButton.style.display = 'none';
+  }
 }
 
-// Add listener to submit element
-submitEl.addEventListener("click", addTip);
+//---------------------------------------------------------------------------
+//MAKE THE BUTTONS WORK
+function showNextSlide() {
+  showSlide(currentSlide + 1);
+}
+
+function showPreviousSlide() {
+  showSlide(currentSlide - 1);
+}
+
+
+//---------------------------------------------------------------------------
+//VARIABLES
+var quizContainer = document.getElementById('quiz');
+var resultsContainer = document.getElementById('results');
+var submitButton = document.getElementById('submit');
+
+//---------------------------------------------------------------------------
+//QUIZ QUESTIONS
+const myQuestions = [
+  {
+    question: "Who invented JavaScript?",
+    answers: {
+      a: "Douglas Crockford",
+      b: "Sheryl Sandberg",
+      c: "Brendan Eich"
+    },
+    correctAnswer: "c"
+  },
+  {
+    question: "Which one of these is a JavaScript package manager?",
+    answers: {
+      a: "Node.js",
+      b: "TypeScript",
+      c: "npm"
+    },
+    correctAnswer: "c"
+  },
+  {
+    question: "Which tool can you use to ensure code quality?",
+    answers: {
+      a: "Angular",
+      b: "jQuery",
+      c: "RequireJS",
+      d: "ESLint"
+    },
+    correctAnswer: "d"
+  }
+];
+
+// display quiz right away
+buildQuiz();
+
+//Pagination
+const previousButton = document.getElementById("previous");
+const nextButton = document.getElementById("next");
+const slides = document.querySelectorAll(".slide");
+let currentSlide = 0;
+showSlide(currentSlide);
+
+//first slide display
+showSlide(currentSlide);
+
+//---------------------------------------------------------------------------
+//Event Listeners
+submitButton.addEventListener('click', showResults);
+previousButton.addEventListener("click", showPreviousSlide);
+nextButton.addEventListener("click", showNextSlide);
